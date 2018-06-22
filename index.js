9460c795e07da0888f126d489a68ea216b599574
Tyler Long <tyler4long@gmail.com>
2018-06-22 16:57:58 +0800

    First working version

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-export const parse = () => {
-  return null
+export const parse = data => {
+  const separator = data.trim().split('\n')[0].trim()
+  const parts = data.split(separator).map(part => part.trim()).filter(part => part !== '' && part !== '--')
+  return parts.map(part => JSON.parse(part.split('\n')[3]))
 }
 
 export default { parse }
